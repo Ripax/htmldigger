25ce3dc935162aa4df726a171ab3cbce413dc9cf
HTMLDigger <HTMLDigger@gmail.com>
2021-11-03 20:15:08 +0530

    added initial code

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# Author : HTMLDigger
+# Date : 03/11/2021
+
+clear
+echo "Welcone to my GIT project.."
+date
+
+printf "***********************************************************\n"
+
+
+
